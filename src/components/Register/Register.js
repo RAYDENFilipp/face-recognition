374606d965022a5524b75ab64ef93525109c2574
Philipp Lypniakov <filipprayden@gmail.com>
2019-01-04 20:20:46 +0200

    Created Form component to optimize Register and SignIn components

--- src/components/Register/Register.js
+++ src/components/Register/Register.js
@@ -1,5 +1,5 @@
 import React from 'react';
-
+import Form from '../Form/Form';
 class Register extends React.Component {
   constructor(props) {
     super(props);
@@ -30,7 +30,7 @@ class Register extends React.Component {
       body: JSON.stringify({
         email: this.state.email,
         password: this.state.password,
-        name: this.state.name,
+        name: this.state.name
       })
     })
       .then(response => response.json())
@@ -44,59 +44,13 @@ class Register extends React.Component {
 
   render() {
     return (
-      <div className="br3 ba b--black-10 mv4 w-100 w-50-m w-25-l mw6 shadow-5 center">
-        <main className="pa4 black-80">
-          <div className="measure">
-            <fieldset id="sign_up" className="ba b--transparent ph0 mh0">
-              <legend className="f1 fw6 ph0 mh0">Register</legend>
-              <div className="mt3">
-                <label className="db fw6 lh-copy f6" htmlFor="name">
-                  Name
-                </label>
-                <input
-                  className="pa2 input-reset ba bg-transparent hover-bg-black hover-white w-100"
-                  type="text"
-                  name="name"
-                  id="name"
-                  onChange={this.onNameChange}
-                />
-              </div>
-              <div className="mt3">
-                <label className="db fw6 lh-copy f6" htmlFor="email-address">
-                  Email
-                </label>
-                <input
-                  className="pa2 input-reset ba bg-transparent hover-bg-black hover-white w-100"
-                  type="email"
-                  name="email-address"
-                  id="email-address"
-                  onChange={this.onEmailChange}
-                />
-              </div>
-              <div className="mv3">
-                <label className="db fw6 lh-copy f6" htmlFor="password">
-                  Password
-                </label>
-                <input
-                  className="b pa2 input-reset ba bg-transparent hover-bg-black hover-white w-100"
-                  type="password"
-                  name="password"
-                  id="password"
-                  onChange={this.onPasswordChange}
-                />
-              </div>
-            </fieldset>
-            <div className="">
-              <input
-                onClick={this.onSubmitSignIn}
-                className="b ph3 pv2 input-reset ba b--black bg-transparent grow pointer f6 dib"
-                type="submit"
-                value="Register"
-              />
-            </div>
-          </div>
-        </main>
-      </div>
+      <Form
+        formName='Register'
+        onEmailChange={this.onEmailChange}
+        onPasswordChange={this.onPasswordChange}
+        onSubmitSignIn={this.onSubmitSignIn}
+        onNameChange={this.onNameChange}
+      />
     );
   }
 }
--- src/components/Signin/Signin.js
+++ src/components/Signin/Signin.js
@@ -1,4 +1,5 @@
 import React from 'react';
+import Form from '../Form/Form';
 
 class Signin extends React.Component {
   constructor(props) {
@@ -38,47 +39,12 @@ class Signin extends React.Component {
 
   render() {
     return (
-      <div className="br3 ba b--black-10 mv4 w-100 w-50-m w-25-l mw6 shadow-5 center">
-        <main className="pa4 black-80">
-          <div className="measure">
-            <fieldset id="sign_up" className="ba b--transparent ph0 mh0">
-              <legend className="f1 fw6 ph0 mh0">Sign In</legend>
-              <div className="mt3">
-                <label className="db fw6 lh-copy f6" htmlFor="email-address">
-                  Email
-                </label>
-                <input
-                  className="pa2 input-reset ba bg-transparent hover-bg-black hover-white w-100"
-                  type="email"
-                  name="email-address"
-                  id="email-address"
-                  onChange={this.onEmailChange}
-                />
-              </div>
-              <div className="mv3">
-                <label className="db fw6 lh-copy f6" htmlFor="password">
-                  Password
-                </label>
-                <input
-                  className="b pa2 input-reset ba bg-transparent hover-bg-black hover-white w-100"
-                  type="password"
-                  name="password"
-                  id="password"
-                  onChange={this.onPasswordChange}
-                />
-              </div>
-            </fieldset>
-            <div className="">
-              <input
-                onClick={this.onSubmitSignIn}
-                className="b ph3 pv2 input-reset ba b--black bg-transparent grow pointer f6 dib"
-                type="submit"
-                value="Sign in"
-              />
-            </div>
-          </div>
-        </main>
-      </div>
+      <Form
+        formName="Sign In"
+        onEmailChange={this.onEmailChange}
+        onPasswordChange={this.onPasswordChange}
+        onSubmitSignIn={this.onSubmitSignIn}
+      />
     );
   }
 }
