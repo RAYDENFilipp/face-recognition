ce03aa6f086a9ef27ad7681364e7bc0806bd2e4f
Philipp Lypniakov <filipprayden@gmail.com>
2019-07-04 22:19:33 +0300

    Updates

---
+++ precache-manifest.66dd52ae46e543dcd1d285153275c6ea.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = [
+  {
+    "revision": "00a7b039938598a9eb93",
+    "url": "/face-recognition/static/js/runtime~main.00a7b039.js"
+  },
+  {
+    "revision": "cd76572738e256de3206",
+    "url": "/face-recognition/static/js/main.cd765727.chunk.js"
+  },
+  {
+    "revision": "330dd59aaa69c57cbae0",
+    "url": "/face-recognition/static/js/1.330dd59a.chunk.js"
+  },
+  {
+    "revision": "cd76572738e256de3206",
+    "url": "/face-recognition/static/css/main.75c04fe1.chunk.css"
+  },
+  {
+    "revision": "330dd59aaa69c57cbae0",
+    "url": "/face-recognition/static/css/1.d4cd1c5f.chunk.css"
+  },
+  {
+    "revision": "4e6c0347009652a9d53db12f3c9ceecd",
+    "url": "/face-recognition/index.html"
+  }
+];
